00dd0fc020fef592ce2363d943572bc9338f3c8b
mdegroot03 <max.de.groot.mdg@icloud.com>
2025-03-18 12:55:02 +0100

    Refactor UI and backend to work with new experiment structure (WIP)

---
+++ preprocessing/src/models/visualization_config.py
@@ -0,0 +1,27 @@
+from models.experiment import Experiment
+from models.group import Group
+from models.types.measurement_type import MeasurementType
+from models.types.experiment_type import ExperimentType
+
+
+class VisualizationConfig:
+    """
+    Data class representing visualization configuration.
+    """
+    name: str
+    experiment: Experiment
+
+    def __init__(self, name: str, groups: [Group], measurement_types: [MeasurementType], experiment_type: ExperimentType):
+        """
+        Construct visualization configuration from request body or folder path.
+        """
+        self.name = name
+        self.experiment = Experiment(groups, experiment_type, measurement_types)
+
+    def to_dict(self) -> dict:
+        """
+        Convert configuration to dictionary parseable by frontend.
+
+        :return: Dictionary representation of configuration.
+        """
+        return {'name': self.name, 'type': str(self.experiment.experiment_type)}
